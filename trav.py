7ad3a36f5bfad7aed98e6f5feeba104b9303660c
Mohaiminul Islam <36233551+mmislam1@users.noreply.github.com>
2024-03-16 17:32:22 +0600

    Update trav.py
    For traversing binary tree

--- trav.py
+++ trav.py
@@ -1,12 +1,16 @@
 s=[root]
-def itr():
-  if len(s)==0:
-    return 0
-  x=s[0]
-  s.pop(0)
-  if x==None:
-    return 0
+def itr2():
+    if len(s)==0:
+        return 0
+    x=s[0]
+    s.pop(0)
             
+    if x==None:
+        itr2()
+                
+    s.append(x.left)
+    s.append(x.right)
             
                 
-  itr()
+    itr2()
+        
